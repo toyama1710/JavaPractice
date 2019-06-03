bf4f4cb42f35e86812e29b00993601198ad87ffe
toyama1710 <walker2002226@gmail.com>
2019-06-03 13:46:18 +0900

    Update

--- FastScanner.java
+++ FastScanner.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class FastScanner {
